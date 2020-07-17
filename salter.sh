2fcb32c6c59e11ddc13c3e7f6b647922cfc8ba1b
N <noel.mcloughlin@gmail.com>
2020-07-17 12:46:03 +0100

    fix(yum): do not get blocked by yum stuff

--- salter.sh
+++ salter.sh
@@ -120,7 +120,8 @@ pkg-add() {
              elif [ -f "/usr/bin/dnf" ]; then
                  /usr/bin/dnf install -y --best --allowerasing ${PACKAGES} || exit 1
              elif [ -f "/usr/bin/yum" ]; then
-                 /usr/bin/yum update -y || exit 1
+		 # centos/rhel has older package versions so allow newer upstream ones (skip-broken)
+                 /usr/bin/yum update -y --skip-broken || exit 1
                  /usr/bin/yum install -y ${PACKAGES} --skip-broken || exit 1
              elif [[ -f "/usr/bin/apt-get" ]]; then
                  /usr/bin/apt-get update --fix-missing -y || exit 1
@@ -152,8 +153,9 @@ pkg-update() {
                  /usr/bin/pacman -Syu --noconfirm "${PACKAGES}" || exit 1
              elif [ -f "/usr/bin/dnf" ]; then
                  /usr/bin/dnf upgrade -y --allowerasing "${PACKAGES}" || exit 1
+		 # centos/rhel has older package versions so allow newer upstream ones (skip-broken)
              elif [ -f "/usr/bin/yum" ]; then
-                 /usr/bin/yum update -y "${PACKAGES}" || exit 1
+                 /usr/bin/yum update -y "${PACKAGES}" --skip-broken || exit 1
              elif [[ -f "/usr/bin/apt-get" ]]; then
                  /usr/bin/apt-get upgrade -y "${PACKAGES}" || exit 1
              elif [[ -f "/usr/sbin/pkg" ]]; then
@@ -293,7 +295,7 @@ salt-bootstrap() {
              if [ -f "/usr/bin/dnf" ]; then
                  pkg-add ${PACKAGES} 2>/dev/null
              elif [ -f "/usr/bin/yum" ]; then
-                 # centos/rhel have many old package versions so we allow newer upstream packages
+		 # centos/rhel has older package versions so allow newer upstream ones (skip-broken)
                  pkg-add ${PACKAGES} --skip-broken 2>/dev/null
              else
                  pkg-add ${PACKAGES} 2>/dev/null
