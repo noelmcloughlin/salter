49d2f313217ad7f27e1f68a55ba1897ab08207d3
noelmcloughlin <noel.mcloughlin@gmail.com>
2020-12-04 18:23:20 +0000

    feat(script): add go.sh script

--- go.sh
+++ go.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-DOO=sudo [ "$OSTYPE" == 'cygwin' ] && DOO=''
+echo "ready .."
+DOO=sudo && [ "$OSTYPE" == 'cygwin' ] && DOO=''
 SALTER=https://raw.githubusercontent.com/saltstack-formulas/salter/master/salter.sh
 
-curl -LO ${SALTER} && $DOO bash salter.sh add bootstrap -i && $DOO bash salter.sh add salter
+curl -LO ${SALTER} && echo "steady .." && $DOO bash salter.sh add bootstrap -i && echo 'go ..' && $DOO bash salter.sh add salter
 
