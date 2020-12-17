12e1cc6bce66945f7a0b0341ab53957e25979c74
noelmcloughlin <noel.mcloughlin@gmail.com>
2020-12-17 19:34:36 +0000

    feat(proxy): add https_proxy support

--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@ echo "ready .."
 DOO=sudo && [ "$OSTYPE" == 'cygwin' ] && DOO=''
 SALTER=https://raw.githubusercontent.com/saltstack-formulas/salter/master/salter.sh
 
-curl -LO ${SALTER} && echo "steady .." && $DOO bash salter.sh add bootstrap -i && echo 'go ..' && $DOO bash salter.sh add salter
+[[ -z "${https_proxy+x}" ]] || GETPROXY="-x ${https_proxy}"
+curl ${GETPROXY} -LO ${SALTER} && echo "steady .." && $DOO bash salter.sh add bootstrap -i && echo 'go ..' && $DOO bash salter.sh add salter
 rm salter.sh >/dev/null 2>&1
 
