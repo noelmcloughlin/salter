c20fccb9ec771f4ccc6f73ddcb4372b351d1d8a0
noelmcloughlin <noel.mcloughlin@gmail.com>
2020-12-18 10:29:32 +0000

    feat(bootstrap): reuse bs_curl_opts salt variable

--- go.sh
+++ go.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-echo "ready .."
 SU=sudo && [ "$OSTYPE" == 'cygwin' ] && SU=''
 SALTER=https://raw.githubusercontent.com/saltstack-formulas/salter/master/salter.sh
 
 [[ -z "${https_proxy+x}" ]] || export BS_CURL_ARGS="-x ${https_proxy}"
-curl ${BS_CURL_ARGS} -LO ${SALTER} && echo "steady .." && $SU bash salter.sh add bootstrap -i && echo 'go ..' && $SU bash salter.sh add salter
+curl ${BS_CURL_ARGS} -LO ${SALTER} && $SU bash ${BS_CURL_ARGS} salter.sh add bootstrap -i && $SU bash ${BS_CURL_ARGS} salter.sh add salter
