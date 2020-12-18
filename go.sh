fe6dea2260d0f3515ddb6a53dec1a09c4575ff66
noelmcloughlin <noel.mcloughlin@gmail.com>
2020-12-18 11:40:28 +0000

    fix(sudo): pass environment

--- go.sh
+++ go.sh
@@ -2,7 +2,7 @@
 
 SU='sudo -E' && [ "$OSTYPE" == 'cygwin' ] && SU=''
 SALTER=https://raw.githubusercontent.com/saltstack-formulas/salter/master/salter.sh
-BS_CURL_IPV="${BS_CURL_IPV:-'--ipv4'}"
+BS_CURL_IPV="${BS_CURL_IPV:---ipv4}"
 
 [[ -z "${https_proxy+x}" ]] || export BS_CURL_ARGS="-x ${https_proxy} ${BS_CURL_IPV}"
 curl ${BS_CURL_ARGS} -LO ${SALTER} && $SU bash salter.sh add bootstrap -i && $SU bash salter.sh add salter
