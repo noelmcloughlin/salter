20803ed6147a7cc52089f962431971284bfc39c4
noelmcloughlin <noel.mcloughlin@gmail.com>
2021-09-09 17:35:54 +0100

    fix(windows): pass windows username

--- go.sh
+++ go.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-SU='sudo -E' && [ "$OSTYPE" == 'cygwin' ] && SU=''
+SU='sudo -E' && [ "$OSTYPE" == 'cygwin' ] && SU='' && USER="-u $(id -un)"
 SALTER=https://raw.githubusercontent.com/saltstack-formulas/salter/master/salter.sh
 BS_CURL_IPV="${BS_CURL_IPV:---ipv4}"
 BS_CURL_ARGS=
 
 [[ -z "${https_proxy+x}" ]] || export BS_CURL_ARGS="-x ${https_proxy} ${BS_CURL_IPV}"
-curl ${BS_CURL_ARGS} -LO ${SALTER} && $SU bash salter.sh add bootstrap -i && $SU bash salter.sh add salter
+curl ${BS_CURL_ARGS} -LO ${SALTER}
+if (( $? == 0 )); then
+    ${SU} bash salter.sh add bootstrap -i ${USER}
+    ${SU} bash salter.sh add salter ${USER}
+fi
